1093411be1c6027d118be0efbdb3a9546d9384ce
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-13 23:49:26 -0600

    Styles Login and Signin Components

---
+++ src/Login/Login.jsx
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { selectUser, getFavorites } from '../../apiCalls/apiCalls';
+import { setUser, setError, setFavorites } from '../../actions/index';
+import { connect } from 'react-redux';
+import './Login.css';
+import { Redirect } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+
+export class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isLoggedIn: false
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleClick = async () => {
+    const { setUser, setError, loadMovieData } = this.props;
+    try {
+      let foundUser = await selectUser(this.state);
+      setUser(foundUser);
+      this.findUserFavorites(foundUser);
+      this.setState({ isLoggedIn: true });
+      loadMovieData();
+      setError('');
+    } catch ({ message }) {
+      setError(message);
+    }
+    this.clearInputs();
+  };
+
+  clearInputs = () => {
+    const stateKeys = Object.keys(this.state);
+    stateKeys.forEach(stateKey =>
+      this.setState({
+        [stateKey]: ''
+      })
+    );
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.handleClick();
+  };
+
+  findUserFavorites = async user => {
+    const { setFavorites } = this.props;
+    if (user.id) {
+      try {
+        let favorites = await getFavorites(user.id);
+        setFavorites(favorites.favorites);
+      } catch ({ message }) {
+        setError(message);
+      }
+    }
+  };
+
+  render() {
+    if (this.state.isLoggedIn) {
+      return <Redirect to='/' />;
+    }
+    const { email, password } = this.state;
+    const { errMsg } = this.props;
+    return (
+      <form className='form-model' onSubmit={e => this.handleSubmit(e)}>
+        {errMsg ? (
+          <p className='login-message login-error'>{errMsg}</p>
+        ) : (
+          <p className='login-message'>
+            Sign in with your email to keep track of your favorite movies.
+          </p>
+        )}
+        <div>
+          <label htmlFor='email' className='Form__label'>
+            Your email
+          </label>
+          {errMsg ? (
+            <input
+              id='email'
+              type='text'
+              placeholder='your@email.com'
+              className='Form__input--error Form__input'
+              name='email'
+              value={email}
+              onChange={e => this.handleChange(e)}
+            ></input>
+          ) : (
+            <input
+              id='email'
+              type='text'
+              placeholder='your@email.com'
+              className='Form__input'
+              name='email'
+              value={email}
+              onChange={e => this.handleChange(e)}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label htmlFor='password' className='Form__label'>
+            Your password
+          </label>
+          {errMsg ? (
+            <input
+              id='password'
+              type='password'
+              className='Form__input--error Form__input'
+              name='password'
+              placeholder='Must have at least 8 characters'
+              value={password}
+              onChange={e => this.handleChange(e)}
+            ></input>
+          ) : (
+            <input
+              id='password'
+              type='password'
+              className='Form__input'
+              name='password'
+              placeholder='Must have at least 8 characters'
+              value={password}
+              onChange={e => this.handleChange(e)}
+            ></input>
+          )}
+        </div>
+        <button type='submit' className='Form__button'>
+          Continue
+        </button>
+      </form>
+    );
+  }
+}
+
+export const mapStateToProps = ({ movies, errMsg, isLoading }) => ({
+  movies,
+  errMsg,
+  isLoading
+});
+
+export const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ setUser, setFavorites, setError }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
+
+LoginForm.propTypes = {
+  movies: PropTypes.array,
+  errMsg: PropTypes.string,
+  isLoading: PropTypes.bool,
+  setUser: PropTypes.func,
+  setFavorites: PropTypes.func,
+  hasError: PropTypes.func
+};
--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,14 +1,151 @@
 import React, { Component } from 'react';
-import './SignIn.css'
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../../apiCalls/apiCalls';
+import { setUser, hasEmailError } from '../../actions/index';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import './Signin.css';
+import PropTypes from 'prop-types';
 
-class SignIn extends Component {
+export class SignUpForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      isLoggedIn: false
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleClick = async () => {
+    const { setUser, hasEmailError } = this.props;
+    try {
+      let newUser = await createUser(this.state);
+      setUser(newUser);
+      this.setState({ isLoggedIn: true });
+      hasEmailError('');
+    } catch ({ message }) {
+      hasEmailError(message);
+    }
+    this.clearInputs();
+  };
+
+  clearInputs = () => {
+    const stateKeys = Object.keys(this.state);
+    stateKeys.forEach(stateKey =>
+      this.setState({
+        [stateKey]: ''
+      })
+    );
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.handleClick();
+  };
+
   render() {
+    if (this.state.isLoggedIn) {
+      return <Redirect to='/' />;
+    }
+    const { name, email, password } = this.state;
+    const { emailErrMsg } = this.props;
     return (
-      <div>
-        
-      </div>
+      <form className='form-model' onSubmit={e => this.handleSubmit(e)}>
+        {emailErrMsg ? (
+          <p className='email-error signup-message'>{emailErrMsg}</p>
+        ) : (
+          <p className='signup-message'>
+            Create an account with your email to keep track of your favorite
+            movies.
+          </p>
+        )}
+        <div>
+          <label htmlFor='email' className='Form__label'>
+            Your name
+          </label>
+          <input
+            id='name'
+            type='text'
+            placeholder='Walter White'
+            className='Form__input Form__input--name'
+            name='name'
+            value={name}
+            onChange={e => this.handleChange(e)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor='email' className='Form__label'>
+            Your email
+          </label>
+          {emailErrMsg ? (
+            <input
+              id='email'
+              type='text'
+              placeholder='your@email.com'
+              className='Form__input--error Form__input'
+              name='email'
+              value={email}
+              onChange={e => this.handleChange(e)}
+            ></input>
+          ) : (
+            <input
+              id='email'
+              type='text'
+              placeholder='your@email.com'
+              className='Form__input Form__input--email'
+              name='email'
+              value={email}
+              onChange={e => this.handleChange(e)}
+            ></input>
+          )}
+        </div>
+        <div>
+          <label htmlFor='password' className='Form__label'>
+            Your password
+          </label>
+          <input
+            id='password'
+            type='password'
+            placeholder='Must have at least 8 characters'
+            className='Form__input Form__input--password'
+            name='password'
+            value={password}
+            onChange={e => this.handleChange(e)}
+          ></input>
+        </div>
+        <button type='submit' className='Form__button signup-form__button'>
+          Continue
+        </button>
+      </form>
     );
   }
 }
 
-export default SignIn;
+export const mapStateToProps = ({ emailErrMsg, isLoading }) => ({
+  emailErrMsg,
+  isLoading
+});
+
+export const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ setUser, hasEmailError }, dispatch);
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignUpForm);
+
+SignUpForm.propTypes = {
+  emailErrMsg: PropTypes.string,
+  isLoading: PropTypes.bool,
+  setUser: PropTypes.func,
+  hasEmailError: PropTypes.func
+};
